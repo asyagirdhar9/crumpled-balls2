e2c1643eecc639497381cf200cc351f38f29b4ed
asyagirdhar9 <73106338+asyagirdhar9@users.noreply.github.com>
2020-10-19 15:07:03 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,20 @@
+class Dustbin{
+  constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      this.image=loadImage("sprites/d.png");
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+       fill("blue");
+  
+      rect(pos.x, pos.y, this.width, this.height);
+     
+    }
+ }
+ 
